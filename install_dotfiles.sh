5d0744401881c8c7c316614680a18db87c24c5be
Onur Ozgur OZKAN <onur.ozgur.ozkan@lab2023.com>
2014-12-11 01:25:06 +0200

    Write a very simple install script for dotfiles.

---
+++ install_dotfiles.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+ 
+ln -s $PWD/dotfiles/zshrc $HOME/.zshrc
+ln -s $PWD/dotfiles/gitconfig $HOME/.gitconfig
+ln -s $PWD/dotfiles/gitignore $HOME/.gitignore
+ln -s $PWD/dotfiles/vimrc $HOME/.vimrc
+ 
+echo 'Done ...'
